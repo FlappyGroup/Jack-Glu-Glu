522166d6972ec397b5651a3d9711d609b4848581
Rute <ruteamaro@gmail.com>
2018-06-08 17:35:02 +0100

    obstacles class is been created- first commit to obstacles

--- src/org/academiadecodigo/stormrooters/flappy_bird/Obstacle.java
+++ src/org/academiadecodigo/stormrooters/flappy_bird/Obstacle.java
@@ -3,32 +3,47 @@ package org.academiadecodigo.stormrooters.flappy_bird;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
+import java.util.ArrayList;
+
 public class Obstacle {
 
-    private Rectangle hitBox;
+    private ArrayList<Rectangle> cells;
+    private int cellHeigth;
+    private int cellWidth;
+    private static final int SIZE = 10;
+    private int middleGap;
+
+
+    public Obstacle(int fieldSize) {
+        this.cellWidth = 100;
+        this.cellHeigth = fieldSize / SIZE;
+
+
+        for (int i = 0; i < SIZE; i++) {
+            cells.add()
+        }
+
+    }
+
 
     /**
      * moves all object right
      */
     public void move() {
 
+        for (Rectangle cell : cells) {
+            cell.translate(-1, 0);
+        }
+
     }
 
-    public int getHeight() {
-        return hitBox.getHeight();
+
+    public int getMiddleGap() {
+
+        return middleGap;
     }
 
-    public int getWidth() {
-        return hitBox.getWidth();
-    }
 
-    public int getX() {
-        return hitBox.getX();
-    }
-
-    public int getY() {
-        return hitBox.getY();
-    }
 }
 
 
