89728d761ad791746ad55f14e24ec1d8043bec60
Rute <ruteamaro@gmail.com>
2018-06-12 18:51:44 +0100

    added class game handler

--- src/org/academiadecodigo/stormrooters/flappy_bird/Main.java
+++ src/org/academiadecodigo/stormrooters/flappy_bird/Main.java
@@ -1,13 +1,13 @@
 package org.academiadecodigo.stormrooters.flappy_bird;
 
 import org.academiadecodigo.stormrooters.flappy_bird.game.Game;
+import org.academiadecodigo.stormrooters.flappy_bird.game.GameHandler;
 
 public class Main {
     public static void main(String[] args ) throws InterruptedException{
 
 
-        Game game = new Game();
-        game.init();
-        game.runGame();
+        GameHandler game = new GameHandler();
+        game.gameStart();
     }
 }
---
+++ src/org/academiadecodigo/stormrooters/flappy_bird/game/GameHandler.java
@@ -0,0 +1,24 @@
+package org.academiadecodigo.stormrooters.flappy_bird.game;
+
+public class GameHandler {
+
+    private Game game;
+
+    public GameHandler() {
+
+        this.game = new Game();
+
+    }
+
+    public void gameStart() {
+        game.init();
+
+        while (true){
+
+
+        }
+
+
+    }
+
+}
